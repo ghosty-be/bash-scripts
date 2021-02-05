08c1bb2ab4d0e45e80bd51ba0e05ccbae74e4915
ghosty-be <ghosty.be@gmail.com>
2021-02-05 14:15:30 +0100

    removed restart, added some more start options and changed spacing

--- virtmic.sh
+++ virtmic.sh
@@ -14,42 +14,72 @@ AUDIOFILE=input.mp3
 # trap ctrl-c and call stop()
 trap stop SIGINT
 
+function createvirtualmic()
+{
+  # Load the "module-pipe-source" module to read audio data from a FIFO special file.
+  echo "Creating virtual microphone."
+  pacmd load-module module-pipe-source source_name=virtmic file=${VIRTMICPATH}/virtmic format=s16le rate=16000 channels=1
+
+  # Set the virtmic as the default source device.
+  echo "Set the virtual microphone as the default device."
+  pacmd set-default-source virtmic
+
+  # Create a file that will set the default source device to virtmic for all PulseAudio client applications.
+  echo "default-source = virtmic" > ~/.config/pulse/client.conf
+}
+
 function start()
 {
-# Load the "module-pipe-source" module to read audio data from a FIFO special file.
-echo "Creating virtual microphone."
-pacmd load-module module-pipe-source source_name=virtmic file=${VIRTMICPATH}/virtmic format=s16le rate=16000 channels=1
+  createvirtualmic
+  echo "start your own stream:"
+  echo "ffmpeg -loglevel quiet -re -i ${AUDIOFILE} -f s16le -ar 16000 -ac 1 - > ${VIRTMICPATH}/virtmic"
+  echo "and don't forget to run '$0 stop' when you are finished"
+}
 
-# Set the virtmic as the default source device.
-echo "Set the virtual microphone as the default device."
-pacmd set-default-source virtmic
+function startlooped()
+{
+  createvirtualmic
+  loopmp3
+}
 
-# Create a file that will set the default source device to virtmic for all PulseAudio client applications.
-echo "default-source = virtmic" > ~/.config/pulse/client.conf
+function startplayonce()
+{
+  createvirtualmic
+  playmp3
+  stop
+}
 
-# Write the audio file to the named pipe virtmic. This will block until the named pipe is read.
-echo "Writing audio file to virtual microphone (in a loop)."
-while :
-do 
- ffmpeg -loglevel quiet -re -i ${AUDIOFILE} -f s16le -ar 16000 -ac 1 - > ${VIRTMICPATH}/virtmic 
-done
+function loopmp3()
+{
+  # Write the audio file to the named pipe virtmic. This will block until the named pipe is read.
+  echo "Writing audio file to virtual microphone (in a loop)."
+  echo "press ctrl-c to stop"
+  while :
+  do
+    playmp3
+  done
+}
+
+function playmp3()
+{
+  ffmpeg -loglevel quiet -re -i ${AUDIOFILE} -f s16le -ar 16000 -ac 1 - > ${VIRTMICPATH}/virtmic
 }
 
 # Uninstall the virtual microphone.
 function stop()
 {
-echo ""
-echo "virtmic interrupted... cleaning up..."
-echo "removing virtual microphone"
-pacmd unload-module module-pipe-source
-echo "removing the virtual microphone as the default device."
-rm ~/.config/pulse/client.conf
+  echo ""
+  echo "virtmic stopped... cleaning up..."
+  echo "removing virtual microphone"
+  pacmd unload-module module-pipe-source
+  echo "removing the virtual microphone as the default device."
+  rm ~/.config/pulse/client.conf
 }
 
 function usage()
 {
-echo "Usage: $0 {start|stop|restart}"
-exit 1
+  echo "Usage: $0 {start|startlooped|startplayonce|stop}"
+  exit 1
 }
 
 case "$1" in
@@ -59,10 +89,11 @@ case "$1" in
   stop)
     stop  
   ;;
-  restart)
-    stop
-    sleep 1
-    start
+  startlooped)
+    startlooped
+  ;;
+  startplayonce)
+    startplayonce
   ;;
   *)
     usage
@@ -70,4 +101,3 @@ case "$1" in
 esac
 
 exit 0
-
